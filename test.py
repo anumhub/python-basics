832d7a7222d7d9ed29dff394e34f2d56b1138588
anumhub <anumsharma@Puneets-MacBook-Pro.local>
2025-01-26 20:49:31 -0800

    add variables and datatypes concepts

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
+# variables and datatype concepts
 print("Hello Anum")
+name="Anum"
+age=12
+price=24.99
+
+# print the values
+
+print("my name is: ",name)
+print("my age is: ",age)
+
+# to know the datatype of varaibles
+
+print(type(name))
+print(type(age))
+print(type(price))
