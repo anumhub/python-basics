2d38876626946ce2dbb99779f3173569c998c633
anumhub <anumsharma@Puneets-MacBook-Pro.local>
2025-01-30 16:00:54 -0800

    varaibles

--- Variables.py
+++ Variables.py
@@ -1,16 +1,58 @@
-# variables and datatype concepts
+# comments
+# hash is used for comments for single line
+"this is another way of comment"
+
+# how to print in python
 print("Hello Anum")
-name="Anum"
-age=12
-price=24.99
+
+# how to store values in varaibles
+# examples
+My_Name="Anum"
+age_2028=12
+price22=24.99
 
 # print the values
 
-print("my name is: ",name)
-print("my age is: ",age)
+print("my name is: ",My_Name)
+print("my age is: ",age_2028)
+print("my price is: ",price22)
 
 # to know the datatype of varaibles
 
-print(type(name))
-print(type(age))
-print(type(price))
+print(type(My_Name))
+print(type(age_2028))
+print(type(price22))
+
+# expression execution
+# string and numeric values can operate together with *
+A,B=2,3
+Txt="@"
+print(2*Txt*3)
+
+# string and string can operate with +
+C,D="2",4
+Txt2="@"
+print((C+Txt2)*D)
+
+#result of division operator with 2 integers will be float
+#result of integer division with float and int give displayed as float
+var1= 1
+var2=2
+var4=1.5
+var5=3
+var6=(var4//var5)
+var3=var1/var2
+print (var3) #  0.5
+print(var6) # 0.0
+
+# reminder is negative if denominator is negative
+
+num1= 5
+num2= 2
+num3= -2
+num4 = num1 % num2  # 1
+num5= num1%num3    # -1
+print (num4)
+print (num5)
+
+
